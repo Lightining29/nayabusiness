c50a9d3a823cb1c096e28a53c10c3ebc17769733
Manish kumar <brayw433@gmail.com>
2026-06-05 16:34:30 +0530

    Configure MERN deployment for Vercel

--- server.js
+++ server.js
@@ -208,6 +208,10 @@ if (process.env.NODE_ENV === 'production' || true) {
 }
 
 // Start Server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production' || require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+module.exports = app;

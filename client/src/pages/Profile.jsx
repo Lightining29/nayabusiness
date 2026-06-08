7196e189624cc162dea806c12c76b648e7ab0d73
Manish kumar <brayw433@gmail.com>
2026-06-08 23:14:01 +0530

    Add missing Profile page component

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h2>Profile Page</h2>
+      <p>This is a protected profile page. Add your user details here.</p>
+    </div>
+  );
+};
+
+export default Profile;
